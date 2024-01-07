f28066e5d9eadea23f2bf2937b7c469776b2aa67
mohamedmehdigara <garamehdi57@yahoo.com>
2024-01-07 09:35:20 +0100

    edited files

--- src/components/HungerBar.js
+++ src/components/HungerBar.js
@@ -1,13 +1,18 @@
-// HungerBar.js
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { HungerContainer, HungerLevel } from '../styles'; // Make sure to import or adjust the paths as needed
 
+// Styled component for the HungerBar, directly extending HungerContainer
+const StyledHungerBar = styled.div`
+  /* Add any additional styling for the overall HungerBar container here */
+  /* For example, you can set the display property or add a border */
+`;
+
+// Styled component for the hunger label
 const StyledHungerText = styled.div`
   font-weight: bold;
-  color: var(--hunger-text-color); // Define the text color for the hunger label
+  color: var(--hunger-text-color); // Text color for the hunger label
 `;
 
 const HungerBar = ({ metricName, hunger }) => {
@@ -20,12 +25,14 @@ const HungerBar = ({ metricName, hunger }) => {
       : 'var(--hunger-red)';
 
   return (
-    <HungerContainer>
+    <StyledHungerBar>
       <StyledHungerText>{metricName}:</StyledHungerText>
+      {/* Updated the HungerContainer to use the determined hungerColor */}
       <HungerContainer hunger={hunger} color={hungerColor}>
+        {/* Updated the HungerLevel to use the determined hungerColor */}
         <HungerLevel hunger={hunger}>{hunger}%</HungerLevel>
       </HungerContainer>
-    </HungerContainer>
+    </StyledHungerBar>
   );
 };
 
